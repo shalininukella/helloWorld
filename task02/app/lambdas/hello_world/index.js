69cc810d9bd0816a18b5c8e096bc782af5c6ddcd
Nukella Shalini <nukella_shalini@epam.com>
2025-03-10 11:52:50 +0530

    done everything

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,32 +1,24 @@
 exports.handler = async (event) => {
-    const path = event.rawPath; // The requested path
-    const method = event.requestContext.http.method; // HTTP method used
- 
-    if (path === "/hello" && method === "GET") {
+    // TODO implement
+    //check the path if it is /hello then return response 200 otherwise 400
+    if (event.rawPath !== "/hello" || event.requestContext?.http?.method !== "GET") {
         return {
-            "statusCode": 200,
-            "body": {
-              "statusCode": 200,
-              "message": "Hello from Lambda"
-            },
-            "headers": {
-              "content-type": "application/json"
- 
-            },
-            "isBase64Encoded": false
-          }
+            statusCode: 400,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                statusCode: 400,
+                message: `Bad request syntax or unsupported method. Request path: ${event.rawPath}. HTTP method: ${event.requestContext?.http?.method}`,
+            })
+        };
     }
- 
-    // Return 400 Bad Request for any other endpoint or method
     return {
-        "statusCode": 400,
-        "body": {
-          "statusCode": 400,
-          "message": `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
-        },
-        "headers": {
-          "content-type": "application/json"
-        },
-        "isBase64Encoded": false
-      }
-}
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            statusCode: 200,
+            message: "Hello from Lambda",
+        })
+    };
+};
+ 
+ 
